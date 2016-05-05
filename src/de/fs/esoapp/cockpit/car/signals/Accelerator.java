b9a7606f9449f8cff7ced74bbaf439e4aee13433
Ferdinand Schneider <ferdinand.schneider(at)dozent.dhbw-vs.de>
2016-05-05 22:00:12 +0200

    FS: add offset for accelerator

--- src/de/fs/esoapp/cockpit/car/signals/Accelerator.java
+++ src/de/fs/esoapp/cockpit/car/signals/Accelerator.java
@@ -5,13 +5,21 @@ import de.fs.esoapp.cockpit.car.SignalBus;
 
 public class Accelerator extends DiscreteSensor<AccelerationData> {
 
+	private int offset = 0;
+
 	public Accelerator(SignalBus signalBus) {
 		super(4, Motor.A, signalBus);
 	}
 
 	@Override
-	protected AccelerationData constructSignalData(int tachoCount) {
-		return new AccelerationData(tachoCount < 0 ? 0 : tachoCount * 20);
+	protected AccelerationData constructSignalData(int newTachoCount) {
+		int tachoCount = newTachoCount - offset;
+		if (tachoCount < 0) {
+			offset = newTachoCount;
+		}
+
+		tachoCount = newTachoCount - offset;
+		return new AccelerationData(tachoCount * 20);
 	}
 
 }
